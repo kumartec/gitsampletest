28409d227aa7ad0b133be288bd60a9d0a6562950
Senthilbtl <kumarteck@gmail.com>
2020-02-02 01:47:19 +0000

    First commit

---
+++ src/test/java/pages/test1.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by aikkiasenthil on 02/02/2020.
+ */
+public class test1 {
+
+    @Test
+    public void print(){
+        System.out.println("Test Git flow");
+    }
+}
